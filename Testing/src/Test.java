4035037553fe334d6f70bae977aa0872dabad6f3
Monica Roselind Panneer Selvam <monicap@itp.objectfrontier.com>
2018-01-31 16:58:46 +0530

    Migrating from SVN Repository
    file:///D:/SVN_Test

---
+++ Testing/src/Test.java
@@ -0,0 +1,12 @@
+
+public class Test {
+
+	public static void main(String args[]){
+		
+		StringBuffer connectionStatus = new StringBuffer();
+		connectionStatus.append("ERROR - Testing");
+		System.out.println(connectionStatus.toString());
+		
+	}
+	
+}
